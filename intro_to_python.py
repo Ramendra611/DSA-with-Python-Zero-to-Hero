63b62255da40d667566d4c9c488537d7d93339e8
Ramendra <ramendra9882@gmail.com>
2026-02-10 22:04:05 +0530

    Add files via upload

---
+++ intro_to_python.py
@@ -0,0 +1,18 @@
+# Writing code in a notebook cell
+print("Hello everyone. I am learning python!!!")
+
+# adding two numbers
+a = 5
+b = 10
+print(a + b)
+
+
+## adding some more things here from vscode
+marks_practical = 80
+marks_theory = 40
+theory_weightage = 0.6
+practical_weightage = 0.4
+
+total_marks = theory_weightage*marks_theory + practical_weightage*marks_practical
+
+print("The total marks: ", total_marks)
